8daed6ed691810213e17902c6054aa53c1235135
Ben James <bmjames@gmail.com>
2015-05-21 13:16:23 +0100

    Implement liftBase

--- Test/Hspec/Control.hs
+++ Test/Hspec/Control.hs
@@ -8,11 +8,11 @@ import Control.Monad.Trans.Writer
 import Data.Functor (($>))
 import Test.Hspec.Core.Spec
 
-instance MonadBase IO (SpecM a)
+instance MonadBase IO (SpecM a) where
+  liftBase = runIO
 
 instance MonadBaseControl IO (SpecM a) where
   type StM (SpecM a) r = (r, [SpecTree a])
   liftBaseWith f = runIO (f runSpecM')
     where runSpecM' (SpecM specs) = runWriterT specs
   restoreM (r, specs) = fromSpecList specs $> r
-
